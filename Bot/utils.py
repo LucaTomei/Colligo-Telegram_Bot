75b1c5c9c14c93fac4a4e9d1480a37cfc7ec70fd
LucaTomei1995 <luca.tom1995@gmail.com>
2020-03-25 16:36:55 +0100

    Upload workflow in README

--- Bot/utils.py
+++ Bot/utils.py
@@ -2,13 +2,27 @@ import json
 
 class Utils(object):
 	def __init__(self):
-		self.settings_file_name = "settings.json"
+		self.root_file_folder = "files/"
+		self.settings_file_name = self.root_file_folder + "settings.json"
+
+		self.merchats_file_name = self.root_file_folder + "merchants.json"
 
 
-	def getContentOfSettings(self):	
-		file = open(self.settings_file_name)
+	def getContentOfFile(self, filename):	
+		file = open(filename)
 		content = json.loads(file.read())
 		file.close()
 		return content
 
-	def getToken(self):	return self.getContentOfSettings()['botToken']
+	def getToken(self):
+		return self.getContentOfFile(self.settings_file_name)['botToken']
+
+	""" Ritorna una lista di tutte le categorie di negozio"""
+	def getAllMerchantCategories(self):
+		return self.getContentOfFile(self.merchats_file_name)['availableCategories']
+
+	
+
+if __name__ == '__main__':
+	Utils = Utils()
+	print(Utils.getAllMerchantCategories())
--- README.md
+++ README.md
@@ -4,3 +4,21 @@
 
 - [ ] Registrazione Negozio: Posizione + Descrizione + Link per Contatto
 - [ ] Visualizzazione Negozi su Mappa
+
+
+## Commerciante
+
+- Crea un gruppo e aggiunge il bot come amministratore
+- Il commerciante riferisce al bot che categoria di negozio ha
+- Il commerciante riferisce al bot dove si trova il negozio
+- Il bot gli risponde con un token (anche tranquillamente un numero random (qualsiasi cosa)) da dare ai suoi clienti
+- Salvataggio credenziali utente su file
+- Inserisco una lista della merce che ho ("q per terminare")
+	
+## Cliente
+
+Come interagisco con il commerciante?
+- Apro il bot
+- Inserisco il codice univoco che il bot ha dato al commerciante
+- Gli invio una lista di quello che mi serve
+- Il messaggio verrà recapitato direttamente nella chat del commerciante in cui sono presenti commercianti e clienti
