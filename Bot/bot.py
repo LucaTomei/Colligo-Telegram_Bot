ce4c4d5f0a8d4d1b48af84dc5460e52133da2a5c
LucaTomei1995 <luca.tom1995@gmail.com>
2020-03-26 19:55:02 +0100

    Upload ultime migliorie e fix tastiere

--- Bot/bot.py
+++ Bot/bot.py
@@ -7,6 +7,14 @@ from utils import Utils
 # 	Dopo aver inviato la roba tramite rest devo resettare le variabili locali -----|
 #	self.resetAllVariables()	<--------------------------------------------------|
 
+# rimozione altra categoria 
+
+"""
+"Benvenuto <NomeCliente>, del negozio <NomeNegozio>.
+Sono EasyCollect, il bot che ti accompagnerà nella vendita online della tua attività. 
+Nei pannelli sottostanti potrai selezionare le categorie che descrivono il tuo negozio e condividerne la posizione con i clienti.
+"""
+
 class Bot(object):
 	def __init__(self):
 		self.Utils_obj = Utils()
@@ -29,12 +37,12 @@ class Bot(object):
 
 		self.yes_no_keyboard = [[self.yes_button],[self.no_button]]
 
-		self.category_message = "Seleziona a quale categoria appartiene il tuo negozio.(Max 3)\n*[Premere il pulsante Fine per terminare l'aggiunta]*"
+		self.category_message = "Seleziona a quale categoria appartiene il tuo negozio.(Max 3)\n*[Premere il pulsante Fine per terminare la selezione]*"
 		self.position_message = "Inviami la posizione(clicca sulla spilla e seleziona *Posizione*, quindi seleziona *Invia posizione corrente*)"
 		self.main_message = "Utilizza la tastiera sottostante"
 		self.category_error_message = "Inserire almeno una tra le categorie elencate"
-		self.category_ok_message = "Categorie aggiunte con successo. Il tuo negozio appartiene alle seguenti categorie: "
-		self.base_step = 0
+		
+
 
 		self.added_categories = []
 		self.myLocation = (None, None)
@@ -79,11 +87,6 @@ class Bot(object):
 							elif chat_message == self.button_location:
 								self.bot.sendMessage(chat_id, text=self.position_message,parse_mode= 'Markdown', reply_markup = ReplyKeyboardRemove())
 								self.i_can_send_location = True
-						
-						elif self.base_step == 1:
-							self.bot.sendMessage(chat_id, text="Sei sicuro di voler aggiungere la categoria *" + chat_message + "*?", reply_markup={'keyboard': self.yes_no_keyboard},parse_mode= 'Markdown')
-							self.yes_no_step = (True, chat_message)
-							self.base_step = 0
 
 						elif chat_message in [j for i in self.yes_no_keyboard for j in i] and self.yes_no_step[0]:
 							category = self.yes_no_step[1]
@@ -94,59 +97,65 @@ class Bot(object):
 								if self.count_categories == self.max_categories:
 									self.main_keyboard = [[self.button_location]]
 									self.is_set_categoria = True
-
-								self.bot.sendMessage(chat_id, text="Categoria " + category +" aggiunta con successo", reply_markup={'keyboard': self.categories_keyboard},parse_mode= 'Markdown')
+									toSend = "Ecco le categorie che hai impostato *" + str(self.added_categories) + "*"
+									if self.is_set_location:
+										toSend = toSend + ", questa è la posizione del tuo negozio " + str(self.myLocation[1])
+										self.bot.sendMessage(chat_id, text=toSend, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
+									else:
+										self.bot.sendMessage(chat_id, text=toSend, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
+								else:
+									self.bot.sendMessage(chat_id, text="Categoria *" + category +"* aggiunta con successo [Premere il pulsante *Fine* per terminare la selezione]", reply_markup={'keyboard': self.categories_keyboard},parse_mode= 'Markdown')
 							elif chat_message == self.no_button:
 								self.bot.sendMessage(chat_id, text="Inserire nuovamente la categoria", reply_markup={'keyboard': self.categories_keyboard},parse_mode= 'Markdown')
 							self.yes_no_step = (False, None)
 						elif chat_message in [j for i in self.categories_keyboard for j in i]:
-							if chat_message == self.other_categories_button:
-								self.bot.sendMessage(chat_id, text=self.category_message, reply_markup = ReplyKeyboardRemove(),parse_mode= 'Markdown')
-								self.base_step = 1
-							elif chat_message == self.stop_button:
+							if chat_message == self.stop_button:
 								if len(self.added_categories) > 0:
 									self.is_set_categoria = True
+									if not self.is_set_location:
+										self.main_keyboard = [[self.button_location]]
+										toSend = "Ecco le categorie che hai impostato *" + str(self.added_categories) + "*"
+										self.bot.sendMessage(chat_id, text= toSend, reply_markup = {'keyboard': self.main_keyboard},parse_mode= 'Markdown')
+									else:
+										toSend = "Tutto impostato con successo:\nCategorie del negozio: *" + str(self.added_categories) + "*\nPosizione del negozio: *" + str(self.myLocation) + "*."
+										self.bot.sendMessage(chat_id, text= toSend, reply_markup = ReplyKeyboardRemove(),parse_mode= 'Markdown')
 								else:
 									self.bot.sendMessage(chat_id, text=self.category_error_message, reply_markup = {'keyboard': self.main_keyboard},parse_mode= 'Markdown')
+							elif chat_message == self.other_categories_button:
+								self.added_categories.append(chat_message)
+								self.is_set_categoria = True
+								self.bot.sendMessage(chat_id, text=self.category_error_message, reply_markup = {'keyboard': self.main_keyboard},parse_mode= 'Markdown')
 							if not self.is_set_categoria:
-								if self.base_step != 1:
-									self.base_step = 0
-									if chat_message not in self.added_categories:
-										if chat_message != self.stop_button:
-											self.bot.sendMessage(chat_id, text="Sei sicuro di voler aggiungere la categoria *" + chat_message + "*?", reply_markup={'keyboard': self.yes_no_keyboard},parse_mode= 'Markdown')
-											self.yes_no_step = (True, chat_message)
-									else:
-										self.bot.sendMessage(chat_id, text="Categoria di " + chat_message + " già aggiunta.", reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
-							else:
-								toSend = "Le Categorie sono già impostate *" + str(self.added_categories) + "*"
-								if not self.is_set_location:
-									toSend = toSend + ", Ti manca solo l'invio della posizione"
-									self.main_keyboard = [[self.button_location]]
+								if chat_message not in self.added_categories:
+									if chat_message != self.stop_button:
+										self.bot.sendMessage(chat_id, text="Sei sicuro di voler aggiungere la categoria *" + chat_message + "*?", reply_markup={'keyboard': self.yes_no_keyboard},parse_mode= 'Markdown')
+										self.yes_no_step = (True, chat_message)
 								else:
-									toSend = toSend + ", La tua posizione è " + str(self.myLocation) 
-								self.bot.sendMessage(chat_id, text= toSend, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
+									self.bot.sendMessage(chat_id, text="Categoria di " + chat_message + " già aggiunta.", reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
 						else:
 							self.bot.sendMessage(chat_id, text=self.main_message, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
 
 					# mi hai inviato la posizione
 					elif content_type == 'location' and self.i_can_send_location and not self.is_set_location:
 						self.myLocation = (latitude, longitude) = (msg['location']['latitude'], msg['location']['longitude'])
-						toSend = "Posizione Inviata: [" + str(latitude) + ", " + str(longitude) + "]"
+						toSend = "Posizione Registrata: *[" + str(latitude) + ", " + str(longitude) + "]*"
 						
 						if not self.is_set_categoria:
 							self.main_keyboard = [[self.button_categoty]]
-
-						self.bot.sendMessage(chat_id, text=toSend,parse_mode= 'Markdown', reply_markup={'keyboard': self.main_keyboard})
+							self.bot.sendMessage(chat_id, text=toSend,parse_mode= 'Markdown', reply_markup={'keyboard': self.main_keyboard})
+						else:
+							toSend = "Tutto impostato con successo:\nCategorie del negozio: *" + str(self.added_categories) + "*\nPosizione del negozio: *" + str(self.myLocation) + "*."
+							self.bot.sendMessage(chat_id, text=toSend, parse_mode= 'Markdown', reply_markup=ReplyKeyboardRemove())
 						self.is_set_location = True
 					#  Messaggio di benvenuto appena si accede al bot
 					elif content_type == 'group_chat_created':
-						toSend = "Ciao " + first_name + " " + last_name + ", del negozio " + group_title + "!\nUtilizza la tastiera sottostante per iniziare la registrazione della tua attività."
+						toSend = "Benvenuto *" + first_name + "*  del negozio *" + group_title + "* sono EasyCollect, il bot che ti accompagnerà nella vendita online della tua attività.\n"
+						toSend = toSend + "Attraverso i pannelli sottostanti potrai selezionare le categorie che descrivono il tuo negozio e condividerne la tua posizione con i clienti."
 						self.bot.sendMessage(chat_id, text=toSend, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
 					elif content_type == 'new_chat_member':	pass
 					#  Per qualsiasi altro caso inviami la tastiera
 					else:
 						self.bot.sendMessage(chat_id, text=self.main_message, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
-						#self.bot.sendMessage(chat_id, text=self.main_message, reply_markup={'keyboard': self.main_keyboard},parse_mode= 'Markdown')
 				elif self.is_set_categoria and self.is_set_location:
 					toSend = "Tutto impostato con successo:\nCategorie del negozio: *" + str(self.added_categories) + "*\nPosizione del negozio: *" + str(self.myLocation) + "*."
 					self.bot.sendMessage(chat_id, text=toSend, reply_markup = ReplyKeyboardRemove(),parse_mode= 'Markdown')
@@ -156,6 +165,7 @@ class Bot(object):
 			print("Sei stato buttato fuori dal gruppo")
 		except Exception as e:
 			if "No suggested keys" in str(e):	pass
+			elif "title" in str(e):	pass
 			print("Eccezione non gestita: " + str(e))
 
 	def main(self):
